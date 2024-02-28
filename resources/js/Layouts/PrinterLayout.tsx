a09a82df017d00acb911bb1631d1cd3f2b23bebc
Anderson <andersonbrasil72@gmail.com>
2024-02-28 16:30:29 -0300

    Push280220241636

--- resources/js/Layouts/PrinterLayout.tsx
+++ resources/js/Layouts/PrinterLayout.tsx
@@ -20,8 +20,7 @@ const PrinterLayout = ({ children, backlink }: PrinterLayoutProps) => {
         screen:shadow-lg 
         screen:border 
         screen:border-white 
-        screen:bg-gray-50 
-        m-3
+        screen:bg-gray-50
         "
         >
             <div
@@ -46,12 +45,12 @@ const PrinterLayout = ({ children, backlink }: PrinterLayoutProps) => {
                     <IoArrowBackSharp size={18} />
                     <span className="ml-1">Voltar</span>
                 </Link>
-                <button onClick={() => print()} className="flex item-center justify-center py-2 px-6 bg-gray-200 rounded-md shadow border border-gray-300 text-sm text-gray-600">
+                {/* <button onClick={() => print()} className="flex item-center justify-center py-2 px-6 bg-gray-200 rounded-md shadow border border-gray-300 text-sm text-gray-600">
                     <IoPrint size={18} />
                     <span className="ml-1">Imprimir</span>
-                </button>
-                {/* <ReactToPrint
-                    bodyClass="mx-[5.7mm] my-[12.5mm] w-[21cm] h-[29.7cm]"
+                </button> */}
+                <ReactToPrint
+                    bodyClass="mx-[1cm] my-[1cm]"
                     // pageStyle="@page { size: A4 }"
                     trigger={() => (
                         <button className="flex item-center justify-center py-2 px-6 bg-gray-200 rounded-md shadow border border-gray-300 text-sm text-gray-600">
@@ -60,7 +59,7 @@ const PrinterLayout = ({ children, backlink }: PrinterLayoutProps) => {
                         </button>
                     )}
                     content={() => componentRef.current}
-                /> */}
+                />
             </div>
             <div ref={(el) => (componentRef.current = el)}>
                 {children}
---
+++ resources/js/Pages/Printer/printer.tsx
@@ -0,0 +1,243 @@
+import { maskCpfCnpj } from "@/Utils/mask";
+import moment from "moment";
+import React from "react";
+
+interface PrintProps {
+    empresa: any;
+    ordem: any;
+    printer: any;
+    tipo: any;
+}
+
+const PrinterData = ({ empresa, ordem, printer, tipo }: PrintProps) => {
+    return (
+        <div className="text-[10px] text-gray-600">
+            <div className="flex items-start justify-start py-1 px-2 border-b border-gray-200">
+                <div className="flex flex-col items-start justify-start w-full">
+                    <h1>{empresa.razao}</h1>
+                    <h1>{empresa.cnpj}</h1>
+                    <h1>
+                        {empresa.endereco} - {empresa.bairro}
+                    </h1>
+                    <h1>
+                        {empresa.cidade} - {empresa.telefone}
+                    </h1>
+                </div>
+                <div className="flex items-center justify-start w-16">
+                    <img
+                        src={`/storage/images/${empresa.logo ? empresa.logo : "logo.png"}`}
+                        alt=""
+                    />
+                </div>
+            </div>
+            <div className="flex items-center justify-between px-2 py-0.5 bg-gray-200">
+                <div>
+                    <span className="font-semibold mr-2">O.S. N°:</span>
+                    <span>{("00000000" + ordem.id).slice(-8)}</span>
+                </div>
+                <div>
+                    <span className="font-semibold mr-2">
+                        Data de abertura:
+                    </span>
+                    <span>{moment(ordem.created_at).format("DD/MM/YYYY")}</span>
+                </div>
+            </div>
+
+            <div className="flex flex-col items-start justify-start pb-1 border border-gray-200 px-2 py-0.5">
+                <div className="mb-1 flex items-center justify-center w-full">
+                    <h1 className="border-b w-full border-gray-100 uppercase font-semibold text-center">
+                        Dados do cliente
+                    </h1>
+                </div>
+
+                <div className="flex items-start justify-start w-full">
+                    <div className="flex-1">
+                        <h1>
+                            <span className="font-semibold mr-1">Nome:</span>
+                            {ordem.cliente.nome}
+                        </h1>
+                        <h1>
+                            <span className="font-semibold mr-1">
+                                Endereço:
+                            </span>
+                            {ordem.cliente.endereco}
+                            {ordem.cliente.bairro}
+                            {ordem.cliente.cidade}
+                        </h1>
+                    </div>
+                    <div className="flex-1">
+                        <h1>
+                            <span className="font-semibold mr-1">
+                                CPF/CNPJ:
+                            </span>
+                            {maskCpfCnpj(ordem.cliente.cpf)}
+                        </h1>
+                        <h1>
+                            <span className="font-semibold mr-1">Contato:</span>
+                            {ordem.cliente.telefone}
+                            {ordem.cliente.email}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-col items-start justify-start pb-2 border border-gray-200 mt-2 p-2">
+                <div className="mb-1 flex items-center justify-center w-full">
+                    <h1 className="border-b w-full border-gray-100 uppercase font-semibold text-center">
+                        Informações do produto
+                    </h1>
+                </div>
+
+                <div className="flex items-start justify-start w-full">
+                    <div className="flex-1">
+                        <h1>
+                            <span className="font-semibold mr-1">
+                                Equipamento:
+                            </span>
+                            {ordem.equipamento}
+                        </h1>
+                    </div>
+                    <div className="flex-1">
+                        <h1>
+                            <span className="font-semibold mr-1">Modelo:</span>
+                            {ordem.modelo}
+                        </h1>
+                    </div>
+                </div>
+                <div>
+                    <h1>
+                        <span className="font-semibold mr-1">
+                            Estado do equipamento:
+                        </span>
+                        {ordem.estado}
+                    </h1>
+                </div>
+            </div>
+
+            <div className="flex flex-col items-start justify-start pb-2 border border-gray-200 mt-4 p-2">
+                <div className="mb-1 flex items-center justify-center w-full">
+                    <h1 className="border-b w-full border-gray-100 uppercase font-semibold text-center">
+                        Defeito relatado
+                    </h1>
+                    <h1 className="border-b w-full border-gray-100 uppercase font-semibold text-center">
+                        Pré-orçamento
+                    </h1>
+                </div>
+
+                <div className="flex items-start justify-start w-full">
+                    <div className="flex-1">
+                        <h1>{ordem.defeito}</h1>
+                    </div>
+                    <div className="flex-1">
+                        <h1>{ordem.preorcamento}</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-col items-start justify-start pb-2 border border-gray-200 mt-4 p-2">
+                <div className="mb-1 flex items-center justify-center w-full">
+                    <h1 className="border-b w-full border-gray-100 uppercase font-semibold text-center">
+                        Garantias e/ou observações
+                    </h1>
+                </div>
+                <div className="flex items-start justify-start w-full">
+                    <div className="flex-1">
+                        <div className="mb-1">
+                            <p
+                                dangerouslySetInnerHTML={{
+                                    __html: `${printer ? printer : ""}`,
+                                }}
+                            />
+                        </div>
+                        <div>{ordem.obs}</div>
+                    </div>
+                </div>
+            </div>
+
+            {tipo !== "1" && (
+                <div className="flex flex-col items-start justify-start border border-gray-200 mt-4 py-0.5 px-0.5">
+                    <div className="mb-1 flex items-center justify-center w-full">
+                        <h1 className="font-semibold">
+                            {ordem.status == 3
+                                ? "Orçamento"
+                                : "Serviços prestados"}
+                        </h1>
+                    </div>
+                    <div
+                        className={`${ordem.status == 3 ? "" : "border-b"} w-full`}
+                    >
+                        {ordem.status == 3
+                            ? ordem.descorcamento
+                            : ordem.detalhes}
+                    </div>
+                    {ordem.status != 3 && (
+                        <div className="flex items-start justify-between w-full bg-gray-200 px-2">
+                            <div>
+                                <span className="mr-2">
+                                    Valor dos serviços:
+                                </span>
+                                <span className="font-semibold">
+                                    {(ordem.valservico
+                                        ? Number(ordem.valservico)
+                                        : 0
+                                    ).toLocaleString("pt-br", {
+                                        style: "currency",
+                                        currency: "BRL",
+                                    })}
+                                </span>
+                            </div>
+
+                            <div>
+                                <span className="mr-2">
+                                    Valor de peças/produtos:
+                                </span>
+                                <span className="font-semibold">
+                                    {(ordem.valpecas
+                                        ? Number(ordem.valpecas)
+                                        : 0
+                                    ).toLocaleString("pt-br", {
+                                        style: "currency",
+                                        currency: "BRL",
+                                    })}
+                                </span>
+                            </div>
+                            <div>
+                                <span className="mr-2">Valor total:</span>
+                                <span className="font-semibold">
+                                    {(
+                                        Number(ordem.valservico) +
+                                        Number(ordem.valpecas)
+                                    ).toLocaleString("pt-br", {
+                                        style: "currency",
+                                        currency: "BRL",
+                                    })}
+                                </span>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )}
+
+            <div className="flex items-center justify-around mt-8">
+                <div className="w-80 border-t border-gray-400 text-center">
+                    Assinatura Cliente
+                </div>
+                {tipo !== "1" ? (
+                    <div className="w-80 border-t border-gray-400 text-center">
+                        Assinatura Responsável Técnico
+                    </div>
+                ) : (
+                    <div className="w-80"></div>
+                )}
+            </div>
+
+            <div className="mt-4 mb-2 ml-8">
+                <p className="">
+                    {empresa.cidade}, {moment().locale("pt").format("LL")}.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default PrinterData;
