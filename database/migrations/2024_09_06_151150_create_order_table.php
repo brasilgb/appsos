7ca292b380569632c6d17f76bfe7de1ed8f63767
Anderson Brasil <andersonbrasil72@gmail.com>
2024-09-08 19:45:08 -0300

    Push

--- database/migrations/2024_09_06_151150_create_order_table.php
+++ database/migrations/2024_09_06_151150_create_order_table.php
@@ -17,10 +17,10 @@ return new class extends Migration
             $table->string('detalhes')->nullable();
             $table->string('defeito')->nullable();
             $table->text('descorcamento')->nullable();
-            $table->decimal('valorcamento', 15,2)->nullable();
-            $table->decimal('custo', 15,2)->nullable();
-            $table->decimal('valservico', 15,2)->nullable();
-            $table->decimal('valpecas', 15,2)->nullable();
+            $table->string('valorcamento')->nullable();
+            $table->string('custo')->nullable();
+            $table->string('valservico')->nullable();
+            $table->string('valpecas')->nullable();
             $table->string('status')->nullable();
             $table->dateTime('dtentrada')->nullable();
             $table->dateTime('dtentrega')->nullable();
