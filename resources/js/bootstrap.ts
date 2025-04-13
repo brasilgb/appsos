3366688e97378d16d2ce6a6d586e86fa446da787
AndersonBrasil <andersonbrasil72@gmail.com>
2025-04-13 18:57:01 -0300

    Push

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -3,11 +3,14 @@ window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 const apios = axios.create({
-    baseURL: "http://localhost:8000/api/",
-    // baseURL: "https://eplusteutonia.com.br/api/",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  
-  export default apios;
+  // baseURL: "http://localhost:8000/api/",
+  baseURL: "https://eplusteutonia.com.br/api/",
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': '*',
+    "Content-Type": "application/json"
+  },
+});
+
+export default apios;
